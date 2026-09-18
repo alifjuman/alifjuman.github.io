0993bab2da6729c418eafa3a20550acdbc048cc0
alif juman <alifjuman@gmail.com>
2026-09-18 10:39:10 +0700

    Add files via upload

--- apps/silsilah/sw.js
+++ apps/silsilah/sw.js
@@ -1,21 +1,27 @@
 
-const CACHE='silsilah-v13-1';
+const CACHE='silsilah-v14-1';
 const ASSETS=[
-  './silsilah-reader-v13-pwa.html',
+  './silsilah-reader-v14-fixed-print-pdf.html',
   './manifest.json'
 ];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
 self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))) .then(()=>self.clients.claim()));
 });
 self.addEventListener('fetch',e=>{
   const url=new URL(e.request.url);
-  if(url.pathname.includes('silsilah2023.ged') || url.hostname.includes('api.github.com')){
-    // network first for GEDCOM & GitHub API
-    e.respondWith(fetch(e.request).then(r=>{ const clone=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,clone)); return r; }).catch(()=>caches.match(e.request)));
+  if(url.href.includes('raw.githubusercontent.com')){
+    e.respondWith(fetch(e.request).then(r=>{
+      const clone=r.clone();
+      caches.open(CACHE).then(c=>c.put(e.request, clone));
+      return r;
+    }).catch(()=>caches.match(e.request)));
     return;
   }
-  e.respondWith(caches.match(e.request).then(cached=> cached || fetch(e.request).then(r=>{ const clone=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,clone)); return r; }).catch(()=>cached)));
+  e.respondWith(caches.match(e.request).then(cached=>{
+    if(cached) return cached;
+    return fetch(e.request);
+  }));
 });
